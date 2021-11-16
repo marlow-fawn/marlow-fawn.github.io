546b96d3d608170f34aebc750afee50c3fcf9837
Marlow <marlow.fawn@gmail.com>
2021-11-15 20:22:00 -0500

    Restarting website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import {Box, Container, Grid, Typography} from "@material-ui/core";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Box>
+            <Container>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} lg={6}>
+                        <Box>
+                            <img src="/columbo-main-NEW.jpeg" width="100%"/>
+                        </Box>
+
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <Typography variant="h2">
+                            MARLOW FAWN
+                        </Typography>
+                        <Typography variant="caption">
+                            They/Them Pronouns - Malden MA, 02148 - (310) 892-7582 - marlow.fawn@gmail.com
+                        </Typography>
+                        <Typography variant="body1">
+                            Software engineer for <a href="">Thinking Robots Inc.</a> <br/>
+                            Queer. Maker. Musician.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <img src="/776589099607654410.jpeg" width="100%"/>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <Typography variant="h3">
+                            About
+                        </Typography>
+                        <Typography variant="body1">
+                            Roboticist living in the Boston area. Pursuing software engineering with a background in mechanical engineering. Interested in assistive robotics and accessible tech.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <img src="/columbo2.jpeg"  width="100%"/>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <Typography variant="h1">
+                            Projects & Hobbies
+                        </Typography>
+                        <Typography variant="body1">
+                            It'sa me, marlow
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <img src="/columbo3.jpeg"  width="100%"/>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <Typography variant="h1">
+                            Resume
+                        </Typography>
+                        <Typography variant="body1">
+                            It'sa me, marlow
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <img src="/Karkat_Vantas.png"  width="100%"/>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <Typography variant="h1">
+                            Contact
+                        </Typography>
+                        <Typography variant="body1">
+                            It'sa me, marlow
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
+    );
 }
 
 export default App;
