dd49faa6a1277ff0746326da4de389f65aca9e36
Marlow <marlow.fawn@gmail.com>
2025-04-08 20:27:38 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
   },
-    site: 'https://marlow-fawn.github.io',
+    site: 'https://mfawn.net',
   integrations: [react()]
 });
